4e8427a413ac7f0f16d94b1bf98beaa9c42627a1
Nick <sakanasan20@gmail.com>
2025-04-10 13:54:48 +0800

    Create CalculatorTest.java

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+}
